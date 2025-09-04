21dc874abbff97dcf5e8affcf97d827f25e51a3c
anglhz <121195392+anglhz@users.noreply.github.com>
2025-09-04 08:09:02 +0200

    Update Hero.tsx
    Changed hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowDown, Github, Linkedin, Mail } from "lucide-react";
-import heroProfile from "@/assets/hero-profile.jpg";
+import heroProfile from "@/assets/new-hero-profile.png";
 
 const Hero = () => {
   return (
